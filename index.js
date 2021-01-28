0519e4b0c62fb7eaac42b99770a41e5d478cfa2a
Brad <bcoffield@gmail.com>
2021-01-28 11:10:04 -0700

    ..

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const db = require('./queries')
 const port = process.env.PORT || 3000;
 // const { pool } = require("./config");
+const {Pool} = require('pg')
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
