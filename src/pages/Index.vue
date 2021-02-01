dbe1192db8f63f39fb8a68560674f512a8228c71
Brad <bcoffield@gmail.com>
2021-02-01 14:52:53 -0700

    eh

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,12 +10,6 @@ export default {
   name: "PageIndex",
   created() {
     console.log("hiiiiiiiiiiiii");
-    // const url = new URL("http://localhost:8000/db")
-    //  fetch(url)
-    //     .then(res => res.json())
-    //     .then(function(body) {
-    //       console.log(body, "eh!");
-    //     });
 
     axios.get("http://localhost:8000/db").then(resp => {
       console.log(resp.data);
