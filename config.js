55a5c8b086e06ef06f37c9ee04e0c02d15597c43
Brad <bcoffield@gmail.com>
2021-02-01 15:00:32 -0700

    stage from this branch to see how things are

--- config.js
+++ config.js
@@ -2,21 +2,23 @@ require('dotenv').config()
 const {Pool} = require('pg')
 
 // THIS IS THE POOL THAT WORKS LOCALLY. EVENTUALLY FIGURE THEM OUT.
-const isProduction = process.env.NODE_ENV === 'production'
-const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 
-const pool = new Pool({
-  connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
-  ssl: isProduction,
-})
+// const isProduction = process.env.NODE_ENV === 'production'
+// const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
+
+// const pool = new Pool({
+//   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+//   ssl: isProduction,
+// })
 
 
 // THIS IS THE POOL THAT WORKS FOR LIVE
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 module.exports = {pool}                  
