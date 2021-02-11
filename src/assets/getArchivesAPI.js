8ad105586d48e27dbd5dd16ada888ace17d8d70d
Brad <bcoffield@gmail.com>
2021-02-11 09:43:30 -0700

    gets api with collectionname arg

--- src/assets/getArchivesAPI.js
+++ src/assets/getArchivesAPI.js
@@ -1,4 +1,10 @@
-export default () => {
 
-    console.log("ehhhhhhhhhh api!")    
+import { axios } from "boot/axios";
+export default async (collectionName) => {
+    console.log(`--> getArchivesAPI: ${collectionName}`);
+    let res = await axios.get(
+          `https://rmc-library-archives.herokuapp.com/collection/${collectionName}/`
+        ); 
+        return res
+   
 }
