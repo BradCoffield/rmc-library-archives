2a74018592b73723a0fc3b3b22ea1d8181b77080
Brad <bcoffield@gmail.com>
2021-02-16 13:46:22 -0700

    nav bars etc progress

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <q-layout view="lHh Lpr fff" class="bg-primary">
     <q-header elevated class="bg-dark text-grey-1" height-hint="64">
-      <q-toolbar class="GPL__toolbar" style="height: 64px">
+      <q-toolbar class="GPL__toolbar" style=" ">
         <q-btn
           flat
           dense
@@ -213,45 +213,4 @@ export default {
 
 
 
-.GPL
-
-  &__toolbar
-    height: 64px
-
-  &__toolbar-input
-    width: 35%
-
-  &__drawer-item
-    line-height: 24px
-    border-radius: 0 24px 24px 0
-    margin-right: 12px
-
-    .q-item__section--avatar
-      padding-left: 12px
-      .q-icon
-        color: #5f6368
-
-    .q-item__label:not(.q-item__label--caption)
-      color: #3c4043
-      letter-spacing: .01785714em
-      font-size: .875rem
-      font-weight: 500
-      line-height: 1.25rem
-
-    &--storage
-      border-radius: 0
-      margin-right: 0
-      padding-top: 24px
-      padding-bottom: 24px
-
-  &__side-btn
-    &__label
-      font-size: 12px
-      line-height: 24px
-      letter-spacing: .01785714em
-      font-weight: 500
-
-  @media (min-width: 1024px)
-    &__page-container
-      padding-left: 94px
 </style>
